81070447f8c60a46938ae108f2e0b9433c4815df
Sammys <bajasammys25@gmail.com>
2023-12-23 15:32:23 -0800

    adding some styles to introduction section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import Header from "./components/Header/header.jsx";
+import Introduction from "./components/Introduction/introduction.jsx";
+
 
 function App() {
   return (
     <div>
       <Header/>
-      
+      <main>
+      <Introduction/>
+
+
+      </main>
     </div>
   );
 }
---
+++ src/components/Introduction/introduction.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./introduction.module.css";
+import myImg from "../../img/village.png"; /* Need two ../ to get src and img files*/ 
+
+function Introduction(){
+
+return(
+<section className={styles.sectionOne}>
+  <div className={styles.intro}>
+  
+  <div className={styles.text}>
+  <p className={styles.name}>Sam Padilla</p>
+  <p className={styles.prhase}>Crafting Digital Experiences: <br/> Where Design Meets Code <br/>and Imagination Comes to Life </p>
+  </div>
+  
+  <img src={myImg} alt="Village img" />
+
+  </div>
+
+
+</section>
+
+
+
+);
+
+
+}
+export default Introduction;
